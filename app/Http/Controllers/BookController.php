c4119cf84399dde3653e0da3e109e4b4f9ffc471
winnthannhtike <winnthannhtike@gmail.com>
2018-11-13 17:48:43 +0630

    debug

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,6 +11,8 @@ class BookController extends Controller
     public function index() {
 
 //        $book = Book::all();
+        
+        dd(Book::all());
 
         return response()->json([
             'code' => 200,
